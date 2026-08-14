bc0d7fc998c6c0567194d92ed7d28a4a5f8c4ce6
htikkke <htikew574@gmail.com>
2026-08-14 17:16:10 +0630

    correct timeframe object rendering and update dynamic card data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import timeData from "./data.json";
+
+const cardTheme = {
+  Work: { bg: "bg-orange-300", icon: "/src/images/icon-work.svg" },
+  Play: { bg: "bg-blue-300", icon: "/src/images/icon-play.svg" },
+  Study: { bg: "bg-pink-300", icon: "/src/images/icon-study.svg" },
+  Exercise: { bg: "bg-green-300", icon: "/src/images/icon-exercise.svg" },
+  Social: { bg: "bg-purple-300", icon: "/src/images/icon-social.svg" },
+  "Self Care": { bg: "bg-yellow-300", icon: "/src/images/icon-self-care.svg" },
+};
+
 export default function App() {
+  const [timeState, setTimeState] = useState("weekly");
   return (
     // main-container
     <div
@@ -26,195 +39,92 @@ export default function App() {
           <div className="bg-navy-900 p-10 rounded-b-2xl flex flex-col gap-6 max-[1025px]:p-5 max-[430px]:flex-row max-[430px]:justify-evenly">
             <p
               id="daily-rate"
-              className="time-option font-rubik text-base text-white opacity-60 cursor-pointer hover:opacity-100"
+              onClick={() => setTimeState("daily")}
+              className={`font-rubik text-base text-white cursor-pointer transition-opacity ${
+                timeState === "daily"
+                  ? "opacity-100"
+                  : "opacity-60 hover:opacity-100"
+              }`}
             >
               Daily
             </p>
             <p
               id="weekly-rate"
-              className="time-option font-rubik text-base text-white opacity-60 cursor-pointer hover:opacity-100"
+              onClick={() => setTimeState("weekly")}
+              className={`font-rubik text-base text-white cursor-pointer transition-opacity ${
+                timeState === "weekly"
+                  ? "opacity-100"
+                  : "opacity-60 hover:opacity-100"
+              }`}
             >
               Weekly
             </p>
             <p
               id="monthly-rate"
-              className="time-option font-rubik text-base text-white opacity-60 cursor-pointer hover:opacity-100"
+              onClick={() => setTimeState("monthly")}
+              className={`font-rubik text-base text-white cursor-pointer transition-opacity ${
+                timeState === "monthly"
+                  ? "opacity-100"
+                  : "opacity-60 hover:opacity-100"
+              }`}
             >
               Monthly
             </p>
           </div>
         </div>
-        <div className="bg-navy-900 rounded-2xl cursor-pointer group hover:bg-hover-color">
-          <div className="bg-orange-300 flex justify-end rounded-t-2xl">
-            <img src="/src/images/icon-work.svg" className="relative z-0" />
-          </div>
-          <div className="bg-navy-900 rounded-2xl p-5 -mt-5 relative z-10 flex flex-col gap-10 max-[1025px]:p-3 group-hover:bg-hover-color">
-            <div className="flex items-center justify-between">
-              <p className="font-rubik text-lg text-white">Work</p>
-              <img
-                src="/src/images/icon-ellipsis.svg"
-                className="hover:brightness-0 hover:invert cursor-pointer"
-              />
-            </div>
-            <div className="flex flex-col gap-3 max-[430px]:flex-row max-[430px]:items-center max-[430px]:justify-between">
-              <p
-                id="work"
-                className="text-white text-5xl font-rubik font-light"
+        {/* other 2-7 containers */}
+        {timeData.map((item, index) => {
+          const currentHours = item.timeframes[timeState].current;
+          const previousHours = item.timeframes[timeState].previous;
+
+          const getTimeLabel = () => {
+            if (timeState === "daily") return "Last-Day";
+            if (timeState === "weekly") return "Last-Week";
+            if (timeState === "monthly") return "Last-Month";
+          };
+
+          return (
+            <div
+              key={index}
+              className="bg-navy-900 rounded-2xl cursor-pointer group hover:bg-hover-color"
+            >
+              <div
+                className={`${cardTheme[item.title].bg} flex justify-end rounded-t-2xl`}
               >
-                32hrs
-              </p>
-              <p
-                id="previous-work"
-                className="font-rubik text-base text-white opacity-60"
-              >
-                Last-Week - 36hrs
-              </p>
+                <img
+                  src={cardTheme[item.title].icon}
+                  alt={cardTheme[item.title]}
+                  className="relative z-0"
+                />
+              </div>
+              <div className="bg-navy-900 rounded-2xl p-5 -mt-5 relative z-10 flex flex-col gap-10 max-[1025px]:p-3 group-hover:bg-hover-color">
+                <div className="flex items-center justify-between">
+                  <p className="font-rubik text-lg text-white">{item.title}</p>
+                  <img
+                    src="/src/images/icon-ellipsis.svg"
+                    className="hover:brightness-0 hover:invert cursor-pointer"
+                  />
+                </div>
+                <div className="flex flex-col gap-3 max-[430px]:flex-row max-[430px]:items-center max-[430px]:justify-between">
+                  <p
+                    id="work"
+                    className="text-white text-5xl font-rubik font-light"
+                  >
+                    {currentHours}
+                    {currentHours === "1" ? "hr" : "hrs"}
+                  </p>
+                  <p
+                    id="previous-work"
+                    className="font-rubik text-base text-white opacity-60"
+                  >
+                    {getTimeLabel()} - {previousHours}
+                    {previousHours === "1" ? "hr" : "hrs"}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="bg-navy-900 rounded-2xl cursor-pointer group hover:bg-hover-color">
-          <div className="bg-blue-300 flex justify-end rounded-t-2xl">
-            <img src="/src/images/icon-play.svg" className="relative z-0" />
-          </div>
-          <div className="bg-navy-900 rounded-2xl p-5 -mt-5 relative z-10 flex flex-col gap-10 max-[1025px]:p-3 group-hover:bg-hover-color">
-            <div className="flex items-center justify-between">
-              <p className="font-rubik text-lg text-white">Play</p>
-              <img
-                src="/src/images/icon-ellipsis.svg"
-                className="hover:brightness-0 hover:invert cursor-pointer"
-              />
-            </div>
-            <div className="flex flex-col gap-3 max-[430px]:flex-row max-[430px]:items-center max-[430px]:justify-between">
-              <p
-                id="play"
-                className="text-white text-5xl font-rubik font-light"
-              >
-                10hrs
-              </p>
-              <p
-                id="previous-play"
-                className="font-rubik text-base text-white opacity-60"
-              >
-                Last-Week - 8hrs
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="bg-navy-900 rounded-2xl cursor-pointer group hover:bg-hover-color">
-          <div className="bg-pink-300 flex justify-end rounded-t-2xl">
-            <img src="/src/images/icon-study.svg" className="relative z-0" />
-          </div>
-          <div className="bg-navy-900 rounded-2xl p-5 -mt-5 relative z-10 flex flex-col gap-10 max-[1025px]:p-3 group-hover:bg-hover-color">
-            <div className="flex items-center justify-between">
-              <p className="font-rubik text-lg text-white">Study</p>
-              <img
-                src="/src/images/icon-ellipsis.svg"
-                className="hover:brightness-0 hover:invert cursor-pointer"
-              />
-            </div>
-            <div className="flex flex-col gap-3 max-[430px]:flex-row max-[430px]:items-center max-[430px]:justify-between">
-              <p
-                id="study"
-                className="text-white text-5xl font-rubik font-light"
-              >
-                4hrs
-              </p>
-              <p
-                id="previous-study"
-                className="font-rubik text-base text-white opacity-60"
-              >
-                Last-Week - 7hrs
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="bg-navy-900 rounded-2xl cursor-pointer group hover:bg-hover-color">
-          <div className="bg-green-300 flex justify-end rounded-t-2xl">
-            <img src="/src/images/icon-exercise.svg" className="relative z-0" />
-          </div>
-          <div className="bg-navy-900 rounded-2xl p-5 -mt-2 relative z-10 flex flex-col gap-10 max-[1025px]:p-3 group-hover:bg-hover-color">
-            <div className="flex items-center justify-between">
-              <p className="font-rubik text-lg text-white">Exercise</p>
-              <img
-                src="/src/images/icon-ellipsis.svg"
-                className="hover:brightness-0 hover:invert cursor-pointer"
-              />
-            </div>
-            <div className="flex flex-col gap-3 max-[430px]:flex-row max-[430px]:items-center max-[430px]:justify-between">
-              <p
-                id="exercises"
-                className="text-white text-5xl font-rubik font-light"
-              >
-                4hrs
-              </p>
-              <p
-                id="previous-exercises"
-                className="font-rubik text-base text-white opacity-60"
-              >
-                Last-Week - 5hrs
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="bg-navy-900 rounded-2xl cursor-pointer group hover:bg-hover-color">
-          <div className="bg-purple-300 flex justify-end rounded-t-2xl">
-            <img src="/src/images/icon-social.svg" className="relative z-0" />
-          </div>
-          <div className="bg-navy-900 rounded-2xl p-5 -mt-13 relative z-10 flex flex-col gap-10 max-[1025px]:p-3 group-hover:bg-hover-color">
-            <div className="flex items-center justify-between">
-              <p className="font-rubik text-lg text-white">Social</p>
-              <img
-                src="/src/images/icon-ellipsis.svg"
-                className="hover:brightness-0 hover:invert cursor-pointer"
-              />
-            </div>
-            <div className="flex flex-col gap-3 max-[430px]:flex-row max-[430px]:items-center max-[430px]:justify-between">
-              <p
-                id="social"
-                className="text-white text-5xl font-rubik font-light"
-              >
-                5hrs
-              </p>
-              <p
-                id="previous-social"
-                className="font-rubik text-base text-white opacity-60"
-              >
-                Last-Week-10hrs
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="bg-navy-900 rounded-2xl cursor-pointer group hover:bg-hover-color">
-          <div className="bg-yellow-300 flex justify-end rounded-t-2xl">
-            <img
-              src="/src/images/icon-self-care.svg"
-              className="relative z-0"
-            />
-          </div>
-          <div className="bg-navy-900 rounded-2xl p-5 -mt-5 relative z-10 flex flex-col gap-10 max-[1025px]:p-3 group-hover:bg-hover-color">
-            <div className="flex items-center justify-between">
-              <p className="font-rubik text-lg text-white">Self Care</p>
-              <img
-                src="/src/images/icon-ellipsis.svg"
-                className="hover:brightness-0 hover:invert cursor-pointer"
-              />
-            </div>
-            <div className="flex flex-col gap-3 max-[430px]:flex-row max-[430px]:items-center max-[430px]:justify-between">
-              <p
-                id="care"
-                className="text-white text-5xl font-rubik font-light"
-              >
-                2hrs
-              </p>
-              <p
-                id="previous-care"
-                className="font-rubik text-base text-white opacity-60"
-              >
-                Last-Week-2hr
-              </p>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
